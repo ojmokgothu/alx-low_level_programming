f29f048bccbcccc33426f0cc201587db09c43e9b
Ogone <og.mokgothu@gmail.com>
2023-02-17 15:45:38 -0800

    Hello World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main(voi)
+int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of a int: %d byte(s)\n", sizeof(int));
