da1040018375e0b573ea88ecbeb5c84b8c35061d
Ogone <og.mokgothu@gmail.com>
2023-03-12 19:11:31 -0700

    0x0A. C - argc, argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,15 +13,10 @@ int _atoi(char *s)
 	int i, d, n, len, f, digit;
 
 	i = 0;
-
 	d = 0;
-
 	n = 0;
-
 	len = 0;
-
 	f = 0;
-
 	digit = 0;
 
 	while (s[len] != '\0')
@@ -32,18 +27,25 @@ int _atoi(char *s)
 		if (s[i] == '-')
 			++d;
 		if (s[i] >= '0' && s[i] <= '9')
+
 		{
 			digit = s[i] - '0';
+
 			if (d % 2)
-				digit = -digit;
+
+			digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
+
 			if (s[i + 1] < '0' || s[i + 1] > '9')
-		break;
+
+				break;
 		f = 0;
 		}
+
 		i++;
 	}
+
 	if (f == 0)
 	return (0);
 
@@ -54,25 +56,26 @@ int _atoi(char *s)
 * main - multiplies two numbers
 * @argc: number of arguments
 * @argv: array of arguments
-* Return: 0 (Success), 1 (Error)
+*Return: 0 (Success), 1 (Error)
 */
 
 int main(int argc, char *argv[])
 
 {
+
 	int result, num1, num2;
 
 	if (argc < 3 || argc > 3)
 
 	{
-		printf("Error\n");
-		return (1);	
-	}
 
+	printf("Error\n");
+
+		return (1);
+	}
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
-
 	printf("%d\n", result);
 	return (0);
 }
